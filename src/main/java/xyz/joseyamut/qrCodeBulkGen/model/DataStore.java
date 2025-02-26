dab0d4a0a3090ac0889c9878ecca1dd2cb189b04
junyamut <jun.yamut@gmail.com>
2025-02-26 18:38:12 +0800

    Added code for generating QR code; Refactored others;

---
+++ src/main/java/xyz/joseyamut/qrCodeBulkGen/model/DataStore.java
@@ -0,0 +1,12 @@
+package xyz.joseyamut.qrCodeBulkGen.model;
+
+import lombok.Data;
+
+@Data
+public class DataStore {
+
+    public String workbookName;
+    public String sourceDir;
+    public String destinationDir;
+
+}
